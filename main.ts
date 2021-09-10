9af2f67f9ac4fdcca6cb9b373e0d5373a7c085e3
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-09-10 11:56:55 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    Nombre += 1
+    basic.showNumber(Nombre)
 })
+input.onButtonPressed(Button.AB, function () {
+    Nombre = 0
+    basic.showNumber(Nombre)
+})
+input.onButtonPressed(Button.B, function () {
+    Nombre += -1
+    basic.showNumber(Nombre)
+})
+let Nombre = 0
+Nombre = 0
